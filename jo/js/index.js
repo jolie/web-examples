23d1039b2a6b660aecd7796c12133a6234082950
Fabrizio Montesi <famontesi@gmail.com>
2022-10-24 12:17:11 +0200

    mmh

--- jo/js/index.js
+++ jo/js/index.js
@@ -1,1 +1,8 @@
-document.getElementById('hello-webpack').innerText = 'Hello from webpack!'
+const Jo = require("@jolie/jo").Jo
+const JoHelp = require("@jolie/jo").JoHelp
+
+Jo.viewUser( { username: "john" } )
+	.then( response => {
+		document.getElementById( 'hello-webpack' ).innerText = JSON.stringify( response )
+    } )
+	.catch( JoHelp.parseError ).catch( alert );
